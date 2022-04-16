d71a564fe393721be3ff407995f263d983ddd895
AlSult <aliiasult@mail.ru>
2022-04-16 12:50:51 +0200

    changed the text

--- WA-HW1/src/main/database/insert.sql
+++ WA-HW1/src/main/database/insert.sql
@@ -1,43 +1,44 @@
-INSERT INTO amupark.park VALUES
-('AcroparkNY','info@acroparkny.com', 'Via Gradenigo, Padova'),
-('AquaFun','public@aquafun.com', 'Corso Milano, Vicenza'),
-('RideAdventures','customerservice@rideadventures.com', 'Via Parco della Vittoria, Monopoli');
+INSERT INTO lms_admin(email, password)
+VALUES('TORE@admin.com', md5('myPassword'));
 
+INSERT INTO lms_student(name, email, password, gender, birthdate, address, approved, admin_id) VALUES
+('Ahmed Saad','Ahmed.Saad@tore.com',md5('SaadPassword'),'Male',DATE '1995-02-19','Padova', '1', 1),
+('Bahrami Sepide','Bahrami.Sepide@tore.com',md5('SepidePassword'),'Female',DATE '1995-02-19','Padova','1', 1),	
+('Hansen Marit Fredrikke','Hansen.MaritFredrikke@tore.com',md5('MaritPassword'),'Female',DATE '1995-02-19','Padova','1', 1),		
+('Rao Abdul Moeed','Rao.AbdulMoeed@tore.com',md5('MoeedPassword'),'Male',DATE '1995-02-19','Padova','1', 1),			
+('Rehman Abdul','Rehman.Abdul@tore.com',md5('AbdulPassword'),'Male',DATE '1995-02-19','Padova','1', 1),		
+('Sohai Mohammad Muzammil','Sohai.MohammadMuzammil@tore.com',md5('MuzammilPassword'),'Male',DATE '1995-02-19','Padova','1', 1),		
+('Soomro Dodo Khan','Soomro.DodoKhan@tore.com',md5('DKPassword'),'Male',DATE '1995-02-19','Padova','1', 1),
+('Sultanova Aliia','Sultanova.Aliia@tore.com',md5('AliiaPassword'),'Female',DATE '1995-02-19','Padova','1', 1),
+('Zabalawi  Iyad','Zabalawi.Iyad@tore.com',md5('IyadPassword'),'Male',DATE '1995-02-19','Padova','1', 1);
 
-INSERT INTO amupark.account VALUES 
-('guglielmo.faggioli@parkville.com', md5('myPassword'), 'Guglielmo', 'Faggioli', 'admin'),
-('John@acroparkny.com', md5('JonhPassword'), 'John', 'Smith', 'maintainer'),
-('Paul@acroparkny.com', md5('PaulPassword'), 'Paul', 'Black', 'builder');
+INSERT INTO lms_teacher (name, email, password, gender, address) VALUES 
+('Nicola Ferro','Nicola.Ferro@tore.com',md5('SaadPassword'),'Male','Padova');
 
-INSERT INTO amupark.model VALUES 
-('DropTower', 'DropTower Model, 2010, 8 seats, Max load 2000 Kg, link: www.amusementRides.com/rides/DropTower'),
-('Coaster', 'Coaster Model, 2002, 10 double seats (max 20 people), Max load 5000 Kg, link: www.amusementRides.com/rides/Coaster'),
-('Carousel', 'Marry-Go-Round (max 50 people), Max load 5000 Kg, link: www.amusementRides.com/rides/Carousel'),
-('PendulumRide', 'Pendulum Ride 50 double seats, Max load 5000 Kg, link: www.amusementRides.com/rides/PenulumRide'),
-('WaterRide', 'Coaster Model, 2002, 10 double seats (max 20 people), Max load 5000 Kg, link: www.amusementRides.com/rides/Coaster');
+INSERT INTO lms_course(name, description, prerequistie) VALUES
+('Web Applications', 'This course lays the foundations of Web design and development, practicing them through the implementation of an actual application.
+We will learn both server-side and client-side Web technologies and how to build full-stack Web applications and Web services, by using Java servlets, REST Web services, Javascript, CSS3 and HTML5.','Java, Javascript, CSS3, HTML5'),
+('Search Engines', 'This course teaches how to design and develop systems for searching, retrieving, and ranking unstructured information at Web scale.
+We will learn the theoretical and technical foundations of information retrieval, focusing on reference applications like search engines, recommender systems, and social media.', 'Java, good and proactive programming skills and, in particular, the object-oriented paradigm and its design principles.'),
+('Foundations of Databases', 'This course lays the foundations of data management and modelling, providing you with the indispensable knowledge needed for storing and searching structured data.
+We will learn about conceptual database design, the relational model, and the Structured Query Language (SQL), as well as how to employ them to design and develop database applications in real-world scenarios.', 'Basic knowledge of the Java programming language.');
 
-INSERT INTO amupark.ride(description, parkid, modelid) VALUES
-('Kids Drop tower in Acropark - Installed in 2010', 'AcroparkNY', 'DropTower'),
-('Adults Roller coaster in RideAdventures - Installed in 2015', 'RideAdventures', 'Coaster'),
-('Adults Drop tower in AquaFun - Installed in 2015', 'AquaFun', 'DropTower'),
-('Kids Roller coaster in AquaFun - Installed in 2018', 'AquaFun', 'Coaster'),
+INSERT INTO lms_material(topic, text, img_name, img_file, course_id, teacher_id) VALUES
+('topic1', 'learning this and that', null, null, 1, 1),
+('topic1', 'learning this and that', null, null, 2, 1),
+('topic1', 'learning this and that', null, null, 3, 1);
 
-('Kids Pendulum Ride in Acropark - Installed in 2020', 'AcroparkNY', 'PendulumRide'),
-('Adults Roller coaster in RideAdventures - Installed in 2019', 'RideAdventures', 'Coaster'),
-('Adults Carousel in AquaFun - Installed in 2021', 'AquaFun', 'Carousel'),
-('Kids Water Ride in AquaFun - Installed in 2018', 'AquaFun', 'WaterRide'),
+INSERT INTO lms_attend(student_id, course_id) VALUES 
+(1, 1),
+(2, 1),
+(3, 1),
+(4, 1),
+(5, 1),
+(6, 1),
+(7, 1),
+(8, 1),
+(9, 1);
 
-('Kids Water Ride in Acropark - Installed in 2010', 'AcroparkNY', 'WaterRide'),
-('Adults Pendulum Ride in RideAdventures - Installed in 2015', 'RideAdventures', 'PendulumRide'),
-('Adults Drop tower in AquaFun - Installed in 2015', 'AquaFun', 'DropTower'),
-('Kids Carousel in AquaFun - Installed in 2018', 'AquaFun', 'Carousel');
+INSERT INTO lms_message VALUES 
 
-INSERT INTO amupark.device(name, description, type, rideid) VALUES
-('dyn1', 'brakets - drop tower', 'dynamometer', 1),
-('bar1', 'brakets - drop tower', 'barometer', 1),
-('cap1', 'front row - coaster', 'capacitor', 2),
-('cap2', 'back row - coaster', 'capacitor', 2),
-('termo1', 'upper - drop tower', 'termometer', 3),
-('bar1', 'lower - drop tower', 'barometer', 3),
-('hygro1', 'front row - coaster', 'hygrometer', 4),
-('cap1', 'back row - coaster', 'dynamometer', 4);
+INSERT INTO lms_register VALUES 
