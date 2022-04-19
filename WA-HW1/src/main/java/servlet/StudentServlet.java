b5eb09e2b72da56fe42064f1dbbb188077cfd396
Marit <maritfha@stud.ntnu.no>
2022-04-19 11:34:09 +0200

    added structure to dao and servlets

---
+++ WA-HW1/src/main/java/servlet/StudentServlet.java
@@ -0,0 +1,13 @@
+package servlet;
+
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+public class StudentServlet  extends AbstractServlet {
+
+    @Override
+    public void doGet(HttpServletRequest req, HttpServletResponse resp) {
+
+    }
+
+}
