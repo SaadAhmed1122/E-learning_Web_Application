a781e194d3fcdfdb108d174240695fcf1bf98e55
Sepide Bahrami <sepide.bahrami@studenti.unipd.it>
2022-05-19 12:31:56 +0000

    db_schema.sql edited online with Bitbucket

--- WA-HW1/src/main/database/db_schema.sql
+++ WA-HW1/src/main/database/db_schema.sql
@@ -1,7 +1,5 @@
 create database LMS OWNER postgres ENCODING = 'UTF8';
 
-create type valid_gender as ENUM ('Male','Female','Other')
-
 create table lms_admin
 (	admin_id INT generated always as identity,
     email CHAR(128) unique not null,
@@ -15,18 +13,12 @@ create table lms_student
 	name CHAR(128) not null, 
     email CHAR(128) unique not null,
 	password CHAR(128) not null,
-	gender valid_gender not null, -- only 3 options available for user to choose
-    birthdate DATE not null, -- date format: 'year-month-day'
+	gender CHAR(128) not null, 
+    birthdate CHAR(512) not null,
 	address CHAR(512), -- can be null
 	approved BOOL not null, -- bool format: '1' for true & '0' for false
-    admin_id INT not null,
  
-	primary key (student_id), 
-  	constraint studentFK   
-  		foreign key (admin_id)  
-  		references lms_admin (admin_id)  
-  			on delete no action 
- 			on update cascade
+	primary key (student_id) 
 );
 
 create table lms_teacher
@@ -34,7 +26,7 @@ create table lms_teacher
 	name CHAR(128) not null, 
     email CHAR(128) unique not null,
 	password CHAR(128) not null,
-	gender valid_gender not null, -- only 3 options available for user to choose
+	gender CHAR(128) not null, -- only 3 options available for user to choose
 	address CHAR(512), -- can be null
  
 	primary key (teacher_id)
@@ -54,7 +46,7 @@ create table lms_material
 	topic CHAR(128) not null, 
     text TEXT not null,
 	img_name TEXT, -- can be null
- 	img_file BYTEA, -- can be null
+ 	img_file TEXT, -- can be null
     course_id INT not null,
     teacher_id INT not null,
  
@@ -108,23 +100,17 @@ create table lms_message
  			on update cascade
 );
 
-create table lms_register
-( 	admin_id INT not null, 
-    teacher_id INT not null,
+create table lms_assign_courses
+(   teacher_id INT not null,
     course_id INT not null,
  
-	primary key (admin_id, teacher_id, course_id), 
-  	constraint registerFK1   
-  		foreign key (admin_id)  
-  		references lms_admin (admin_id)  
-  			on delete no action
- 			on update cascade,
-   	constraint registerFK2  
+	primary key (teacher_id, course_id), 
+   	constraint assign_coursesFK1 
   		foreign key (teacher_id)  
   		references lms_teacher (teacher_id)  
   			on delete cascade
  			on update cascade,
- 	constraint registerFK3
+ 	constraint assign_coursesFK2
   		foreign key (course_id)  
   		references lms_course (course_id)  
   			on delete cascade
